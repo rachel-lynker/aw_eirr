b4aa50a3637a99f6f57499ac4e8d0d79207a6789
anguswg-ucsb <anguswatters@gmail.com>
2023-02-22 12:04:11 -0800

    get_gate_data script for pulling streamflow data, gage_tbl function for quick lookup of gages

--- R/get_gage_data.R
+++ R/get_gage_data.R
@@ -2,45 +2,66 @@
 # Get Observed streamflows data
 
 # load libraries
-library(dataRetrieval)
+library(cdssr)
 library(dplyr)
 
 source("R/utils.R")
 
-# table of gages
-gage_df <- 
-  gage_tbl()  %>% 
-  dplyr::distinct(river, gage_id) %>% 
-  na.omit()
-gage_df
+# save output path
+save_path <- "data/streamflow/observed/discharge.csv"
 
-# loop over USGS gage IDs and pull together data
-flows_df <- lapply(1:nrow(gage_df), function(i) {
-
-  message(paste0(
-     i, "/", nrow(gage_df),
-     "\nRiver: ",  gage_df$river[i], 
-     "\nReach: ",  gage_df$reach[i],
-     "\nGage ID: ",  gage_df$gage_id[i],
-     "\n--------------------------------------------------"
-     ))
+# check if streamflow discharge daily data already exists, otherwise get data
+if(file.exists(save_path)) {
   
-  tryCatch({
-    
-    df <- dataRetrieval::readNWISdv(
-                      siteNumbers = gage_df$gage_id[i],
-                      parameterCd = '00060',
-                      startDate   = '1980-01-01',
-                      endDate     = Sys.Date()
-                    )
-    df
-    
-  }, error = function(e)
-    
-    NULL
-  )
+  message(paste0("Reading data from:\n---> ", save_path))
   
-}) %>% 
-  dplyr::bind_rows()
+  flow_df <- readr::read_csv(save_path)
+  
+} else {
+  
+  # table of gages
+  gage_df <- 
+    gage_tbl()  %>% 
+    dplyr::distinct(river, gage_id, gage_name, source) 
+  
+  # rm(i, flow_df)
+  flow_df <- lapply(1:nrow(gage_df), function(i) {
+  
+    message(paste0(
+      i, "/", nrow(gage_df),
+      "\nRiver: ",  gage_df$river[i], 
+      "\nUSGS ID: ",  gage_df$gage_id[i],
+      "\nAbbrev: ",  gage_df$gage_name[i],
+      "\n--------------------------------------------------"
+    ))
+    
+    
+    if(gage_df$source[i] == "USGS") {
+      
+      sw <- cdssr::get_sw_ts(
+        usgs_id     = gage_df$gage_id[i],  
+        start_date  = '1980-01-01',
+        end_date    = Sys.Date()
+      )
+      
+      } else {
+        
+        sw <- cdssr::get_sw_ts(
+          abbrev      = gage_df$gage_name[i],  
+          start_date  = '1980-01-01',
+          end_date    = Sys.Date()
+        )
+        
+      }
+    
+    sw
+      
+  
+  }) %>% 
+    dplyr::bind_rows()
+  
+  readr::write_csv(flow_df, save_path)
+  
+}
 
 
--- R/utils.R
+++ R/utils.R
@@ -2,11 +2,6 @@ gage_tbl <- function() {
   
   dplyr::tibble(
    river = c(
-            "Crystal",
-            "Crystal",
-            "Crystal",
-            "Crystal",
-            "Crystal",
             "Roaring Fork",
             "Roaring Fork",
             "Roaring Fork",
@@ -16,31 +11,27 @@ gage_tbl <- function() {
             "Yampa",
             "Yampa",
             "Yampa",
-            "Yampa"
+            "Yampa",
+            "Cache La Poudre",
+            "Cache La Poudre",
+            "Cache La Poudre"
             ),
    reach = c(
-     "Avalanche Creek to B.R.B. Campground",
-     "Penny Hot Springs to Avalanche Creek",
-     "Marble to Redstone",
-     "Road to Crystal to Beaver Lake",
-     "Crystal Mill Falls to Crystal Gorge",
-     "Black Bridge to Veltus Park (Cemetery)",
-     "Basalt to Carbondale",
-     "Lower Woody Creek Bridge to Rte. 82 Bridge",
-     "Slaughterhouse",
-     "Weller Lake to Difficult CG",
-     'Yampa River Park to Transit Center',
-     "Transit Center to Pump Station",
-     'Little Yampa Canyon',
-     "Cross Mountain Gorge - 85 Rd to Deer Lodge Park Rd",
-     'Deerlodge Park to Echo Park'
+     "Black Bridge to Veltus Park (Cemetery)", "Basalt to Carbondale",
+     "Lower Woody Creek Bridge to Rte. 82 Bridge", "Slaughterhouse",  "Weller Lake to Difficult CG",
+     'Yampa River Park to Transit Center', "Transit Center to Pump Station",
+     'Little Yampa Canyon', "Cross Mountain Gorge - 85 Rd to Deer Lodge Park Rd", 'Deerlodge Park to Echo Park',
+     "Filter Plant",  "Poudre Whitewater Park", "Big South"
      ),
+  gage_name = c(
+    "Roaring Fork River at Glenwood Springs, CO", "Roaring Fork near Emma, CO",
+    "Roaring Fork River Blw Maroon Creek Nr Aspen, CO", "Roaring Fork River Blw Maroon Creek Nr Aspen, CO", 
+    "Roaring Fork River Ab Difficult Cr Nr Aspen, CO",
+    "Yampa River at Steamboat Springs, CO", "Yampa River above Elkhead Creek near Hayden, CO", 
+    "Yampa River below Craig, CO", "Yampa River near Maybell, CO",  "Yampa River at Deerlodge Park, CO",
+    "CLAFTCCO", NA, "LAPLODCO"
+  ),
   gage_id = c(
-              "09081600",
-              "09081600",
-              NA,
-              "09081600",
-              "09081600",
               "09085000",
               "09081000",
               "09076300",
@@ -50,8 +41,38 @@ gage_tbl <- function() {
               "09244490",
               "09247600",
               "09251000",
-              "09260050"
-              )
+              "09260050",
+              "06752000",
+              "06752260", 
+              NA
+              ),
+  min_threshold = c(
+    200, 200, 200, 200,100,
+    700, 500, 1100, 700,1300,
+    522.7273, 354.797, 170.1005
+    ),
+  max_threshold = c(
+    10000,1500, 1400, 2700,1000,
+    5000, 5000, 10000, 5000, 25000,
+    1000000, 1000000, 1000000
+  ),
+  source = c(
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "USGS",
+    "CDSS"
+  )
   )
   
 }
+
+# get_boatable_days <- function(df)
